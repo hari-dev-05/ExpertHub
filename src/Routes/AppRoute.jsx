92a678e887e69407102b83c8a1ca6c5344296e92
hari-dev-05 <hariharan040511@gmail.com>
2025-11-06 18:06:01 +0530

    email request completed

--- src/Routes/AppRoute.jsx
+++ src/Routes/AppRoute.jsx
@@ -15,7 +15,7 @@ import ComHome from "../Community/ComHome.jsx";
 import ComProfile from "../Community/ComProfile.jsx";
 import ComPeople from "../Community/ComPeople.jsx";
 import ConnectedPerson from "../Connect/ConnectedPerson.jsx";
-import CheckingProfile from "../Connect/CheckingProfile.jsx";
+
 
 const AppRoute = () => {
   const location = useLocation();
@@ -35,7 +35,8 @@ const AppRoute = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/connected/:userId" element={<ConnectedPerson />} />
-        <Route path="/checkprofile/:userId" element={<CheckingProfile />} />
+   <Route path="/meeting/:userId" element={<Meeting />} />
+
         <Route path="/forget" element={<ForgetPass />} />
 
 
